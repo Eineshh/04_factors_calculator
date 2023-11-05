63e070253aca3f654142127aa809e8775776c6a8
Eineshh <142182572+Eineshh@users.noreply.github.com>
2023-11-06 10:03:15 +1300

    Add files via upload

--- 00_factors_calculator.py
+++ 00_factors_calculator.py
@@ -58,6 +58,21 @@ def num_check(question):
 
 # gets factors, returns a sorted list
 def get_factors(to_factor):
+        # get integer (must be >= 0)
+        var_integer = num_check("Please enter an integer: ", 0)
+
+        var_binary = "{0:b}".format(var_integer)
+
+        # calculate # of bits (length of string above)
+        num_bits = len(var_binary)
+
+        # output answer with working
+        print()
+        print("{} in binary is {}".format(var_integer, var_binary))
+        print("# of bits is {}".format(num_bits))
+        print()
+
+        return ""
 
 
 
